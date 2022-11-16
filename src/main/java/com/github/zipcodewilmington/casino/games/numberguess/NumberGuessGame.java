108c1a3682b3d26eaa8b1a5bb0737e78adcb43e7
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-16 14:15:11 -0500

    created test class for numberGuessGame

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -1,23 +1,38 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
 
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
 import com.github.zipcodewilmington.utils.IOConsole;
 
 /**
  * Created by leon on 7/21/2020.
  */
-public class NumberGuessGame extends IOConsole {
+public class NumberGuessGame extends IOConsole implements GameInterface{
+    public NumberGuessGame() {}
 
     public void main(String[] args) {
+    }
 
+    @Override
+    public void add(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void run() {
         int winningNum = (int) (50 * Math.random());
         int numOfGuesses = 0;
         int numPlayerGuessed;
 
         for (int i = 0; i < 50; i++) {
-            numPlayerGuessed = getIntegerInput("", args);
+            numPlayerGuessed = getIntegerInput("Guess a number");
             if (winningNum == numPlayerGuessed) {
                 System.out.println("Correct!");
-                numOfGuesses++;
                 break;
             } else if (winningNum > numPlayerGuessed) {
                 System.out.println("Too small, guess again");
@@ -25,11 +40,10 @@ public class NumberGuessGame extends IOConsole {
             } else if (winningNum < numPlayerGuessed) {
                 System.out.println("Too big, guess again");
                 numOfGuesses++;
-            } else if (numOfGuesses == 3) {
+            } else if (numOfGuesses == 5) {
                 System.out.println("Out of tries");
             }
         }
 
     }
-
 }
---
+++ src/test/java/com/github/zipcodewilmington/NumberGuessGameTest.java
@@ -0,0 +1,14 @@
+package com.github.zipcodewilmington;
+
+import com.github.zipcodewilmington.casino.games.numberguess.NumberGuessGame;
+import org.junit.Test;
+
+public class NumberGuessGameTest {
+
+//    @Test
+//    public void runTest() {
+//        NumberGuessGame game = new NumberGuessGame();
+//        game.run();
+//    }
+
+}
