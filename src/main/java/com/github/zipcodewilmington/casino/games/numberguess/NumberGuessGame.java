be8fc3e2d0fbdacff6afbdd96a53d6129ac8b0d0
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-16 12:09:17 -0500

    wrote out basic num guess game framework

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -1,7 +1,35 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
 
+import com.github.zipcodewilmington.utils.IOConsole;
+
 /**
  * Created by leon on 7/21/2020.
  */
-public class NumberGuessGame {
+public class NumberGuessGame extends IOConsole {
+
+    public void main(String[] args) {
+
+        int winningNum = (int) (50 * Math.random());
+        int numOfGuesses = 0;
+        int numPlayerGuessed;
+
+        for (int i = 0; i < 50; i++) {
+            numPlayerGuessed = getIntegerInput("", args);
+            if (winningNum == numPlayerGuessed) {
+                System.out.println("Correct!");
+                numOfGuesses++;
+                break;
+            } else if (winningNum > numPlayerGuessed) {
+                System.out.println("Too small, guess again");
+                numOfGuesses++;
+            } else if (winningNum < numPlayerGuessed) {
+                System.out.println("Too big, guess again");
+                numOfGuesses++;
+            } else if (numOfGuesses == 3) {
+                System.out.println("Out of tries");
+            }
+        }
+
+    }
+
 }
